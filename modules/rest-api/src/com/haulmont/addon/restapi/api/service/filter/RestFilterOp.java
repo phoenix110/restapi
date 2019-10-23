172d4d8078ad802af85a47cd32e04829e505e956
Nikita Shchienko <shchienko@haulmont.com>
2019-10-23 19:27:26 +0400

    Introduce isNull entity search condition (34)

--- modules/rest-api/src/com/haulmont/addon/restapi/api/service/filter/RestFilterOp.java
+++ modules/rest-api/src/com/haulmont/addon/restapi/api/service/filter/RestFilterOp.java
@@ -18,46 +18,32 @@
 package com.haulmont.addon.restapi.api.service.filter;
 
 import com.haulmont.chile.core.datatypes.impl.EnumClass;
-import com.haulmont.cuba.core.global.AppBeans;
-import com.haulmont.cuba.core.global.Messages;
 
 public enum RestFilterOp implements EnumClass<String> {
-    CONTAINS("like", "contains", false),
-    EQUAL("=", "==", false),
-    IN("in", "in", false),
-    NOT_IN("not in", "in", false),
-    NOT_EQUAL("<>", "!=", false),
-    GREATER(">", ">", false),
-    GREATER_OR_EQUAL(">=", ">=", false),
-    LESSER("<", "<", false),
-    LESSER_OR_EQUAL("<=", "<=", false),
-    DOES_NOT_CONTAIN("not like", "contains", false),
-    NOT_EMPTY("is not null", "!= null", true),
-    STARTS_WITH("like", "startsWith", false),
-    DATE_INTERVAL("", "", false),
-    ENDS_WITH("like", "endsWith", false),
-    IS_NULL("is null", "== null", false);
+    CONTAINS("like"),
+    EQUAL("="),
+    IN("in"),
+    NOT_IN("not in"),
+    NOT_EQUAL("<>"),
+    GREATER(">"),
+    GREATER_OR_EQUAL(">="),
+    LESSER("<"),
+    LESSER_OR_EQUAL("<="),
+    DOES_NOT_CONTAIN("not like"),
+    NOT_EMPTY("is not null"),
+    STARTS_WITH("like"),
+    DATE_INTERVAL(""),
+    ENDS_WITH("like"),
+    IS_NULL("is null");
 
     private String forJpql;
-    private String forGroovy;
-    private boolean unary;
 
-    RestFilterOp(String forJpql,String forGroovy, boolean unary) {
-        this.forGroovy = forGroovy;
-        this.unary = unary;
+    RestFilterOp(String forJpql) {
         this.forJpql = forJpql;
     }
 
     public String forJpql() {
         return forJpql;
-    }
-
-    public String forGroovy() {
-        return forGroovy;
-    }
-
-    public boolean isUnary() {
-        return unary;
     }
 
     public static RestFilterOp fromJpqlString(String str) {
@@ -66,18 +52,6 @@ public enum RestFilterOp implements EnumClass<String> {
                 return op;
         }
         throw new UnsupportedOperationException("Unsupported operation: " + str);
-    }
-
-    public static RestFilterOp fromGroovyString(String str) {
-        for (RestFilterOp op : values()) {
-            if (op.forGroovy != null && op.forGroovy.equals(str))
-                return op;
-        }
-        throw new UnsupportedOperationException("Unsupported operation: " + str);
-    }
-
-    public String getLocCaption() {
-        return AppBeans.get(Messages.class).getMainMessage("RestFilterOp." + this.name());
     }
 
     @Override
